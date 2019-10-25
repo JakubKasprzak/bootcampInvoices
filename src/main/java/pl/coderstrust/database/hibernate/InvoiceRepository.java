e28cbc2f37a176b760e493d0f38dfd651b163d6b
zlp <matp321@gmail.com>
2019-10-25 23:23:12 +0200

    Task database choose mechanism working only with enabled JPArepository

--- src/main/java/pl/coderstrust/database/hibernate/InvoiceRepository.java
+++ src/main/java/pl/coderstrust/database/hibernate/InvoiceRepository.java
@@ -1,6 +1,7 @@
 package pl.coderstrust.database.hibernate;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface InvoiceRepository extends JpaRepository<HibernateInvoice, Long> {
 }
